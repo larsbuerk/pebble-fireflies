a072d04bddd71f3dd08a0c3bef46038351f419ab
Lars Bürk <larsb@vtxmail.ch>
2015-03-19 20:18:19 +0100

    watchface converted for SDK 3.0

---
+++ build/aplite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 2, 1 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Fireflies",
+  .company = "Little Hiccup",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x24, 0xFC, 0xF7, 0xE1, 0xBD, 0xCD, 0x4C, 0x34, 0x80, 0x04, 0x62, 0x37, 0xF5, 0x08, 0x74, 0x61 },
+  .virtual_size = 0xb6b6
+};
